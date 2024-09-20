3cc135f22954fd2f4d85a758e24e7cda28497c27
Giselle Martinez-Sanchez <77756063+GiselleMtnezS@users.noreply.github.com>
2024-09-19 20:40:20 -0500

    basic setup

---
+++ portfolio/components/footer.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const navigation: any[] = []
+
+export default function Footer() {
+  return (
+    <footer className='py-8'>
+      <div className='container max-w-3xl'>
+        <div className='md:flex md:items-center md:justify-between'>
+          <div className='flex justify-center space-x-6 md:order-2'>
+            {navigation.map(item => (
+              <a
+                key={item.name}
+                href={item.href}
+                target='_blank'
+                rel='noreferrer noopener'
+                className='text-muted-foreground hover:text-foreground'
+              >
+                <span className='sr-only'>{item.name}</span>
+                <item.icon aria-hidden='true' className='h-5 w-5' />
+              </a>
+            ))}
+          </div>
+          <div className='mt-8 md:order-1 md:mt-0'>
+            <p className='text-center text-xs leading-5 text-muted-foreground'>
+              &copy; {new Date().getFullYear()} Your Company. All rights
+              reserved.
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  )
+}
