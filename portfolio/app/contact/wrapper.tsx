59c5a2cef67178c6f5ff7d01ed4dbea21377cf4c
Giselle Martinez-Sanchez <77756063+GiselleMtnezS@users.noreply.github.com>
2024-10-10 12:04:55 -0500

    resetting until content is ready

---
+++ portfolio/app/contact/wrapper.tsx
@@ -0,0 +1,11 @@
+import ContactForm from '@/components/contact/contact-form'
+
+export default function ContactFormWrapper() {
+  return (
+    <div className='container max-w-3xl'>
+      <h2 className='title'>Let&apos;s talk about your project</h2>
+
+      <ContactForm />
+    </div>
+  )
+}
--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -1,9 +1,10 @@
 import Intro from '@/components/home/intro'
-import NewsletterForm from '@/components/home/newsletter-form'
-import RecentPosts from '@/components/posts/recent-posts'
-import RecentProjects from '@/components/projects/recent-projects'
+// import NewsletterForm from '@/components/home/newsletter-form'
+// import RecentPosts from '@/components/posts/recent-posts'
+// import RecentProjects from '@/components/projects/recent-projects'
 
 import { MDXRemote } from 'next-mdx-remote/rsc';
+import ContactFormWrapper from './contact/wrapper';
 
 export default function Home() {
   const content = `
@@ -14,10 +15,10 @@ export default function Home() {
       <div className='container max-w-3xl'>
         <Intro />
         <MDXRemote source={content} />
-        <RecentPosts />
-        <RecentProjects />
-
-        <NewsletterForm />
+        {/* <RecentPosts /> */}
+        {/* <RecentProjects /> */}
+        <ContactFormWrapper />
+        {/* <NewsletterForm /> */}
       </div>
     </section>
   )
